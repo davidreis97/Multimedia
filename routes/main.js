f96df76500cdb0865f0efb3a90c7abc6b0eb2a95
Marisa Oliveira <up201308594@fe.up.pt>
2019-05-06 16:25:11 +0100

    It is kinda a piano

--- routes/main.js
+++ routes/main.js
@@ -4,5 +4,9 @@ let router = new express.Router();
 router.get('/', function(req, res, next) {
     res.render('index');
 });
+
+router.get('/piano', function(req, res, next) {
+    res.render('piano');
+});
   
 module.exports = router;
